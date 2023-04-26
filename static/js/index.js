0488775ddc7818ba442d2986eb0a6e19ca03a116
rabi basukala <ibarbasukala77@gmail.com>
2023-04-26 09:33:05 +0545

    enable/disable submitbtn js

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,16 @@
 console.log("js load");
+
+var inputRadio = document.querySelectorAll("input[type='radio']");  // Get all radio buttons
+
+
+// JavaScript code for enabling/disabling the submit button
+for (var i = 0; i < inputRadio.length; i++) {
+    inputRadio[i].addEventListener("change", function () {
+        var selectedAnswers = document.querySelectorAll('input[type="radio"]:checked'); // Get all selected answers
+        if (selectedAnswers.length === inputRadio.length / 4) {
+            document.getElementById("submitBtn").disabled = false; // Enable the submit button
+        } else {
+            document.getElementById("submitBtn").disabled = true; // Disable the submit button
+        }
+    });
+}
